4275e63ece48c2ff2ae7e214c281c615fedf903a
Lucas Petry <lucasmaypetry@gmail.com>
2015-03-13 13:09:46 -0300

    Configuração das ações dos botões de zoom.

--- CG-SGI/include/gui/forms/MainWindow.h
+++ CG-SGI/include/gui/forms/MainWindow.h
@@ -3,11 +3,19 @@
 
 #include "Window.h"
 #include "ui/ui_MainWindow.h"
+#include <iostream>
 
 class MainWindow : public Window, private Ui::MainWindow {
 
 public:
-	MainWindow(QDialog *parent = 0);
+	MainWindow(QDialog *parent = 0, Qt::WindowFlags flags = Qt::Widget);
+
+protected:
+	void connectSignalsAndSlots();
+
+private slots:
+     void on_btnZoomIn_pressed();
+     void on_btnZoomOut_pressed();
 
 };
 
--- CG-SGI/src/gui/forms/Window.cpp
+++ CG-SGI/src/gui/forms/Window.cpp
@@ -1,6 +1,6 @@
 #include "gui/forms/Window.h"
 
-Window::Window(QDialog *parent) : QDialog(parent) { }
+Window::Window(QDialog *parent, Qt::WindowFlags flags) : QDialog(parent, flags) { }
 
 void Window::moveToCenter() {
 	QRect position = frameGeometry();
