d8ab1fec0398caaa97c61aaeb90c7a47c7fa70cb
Lucas Petry <lucasmaypetry@gmail.com>
2015-03-18 23:45:44 -0300

    Fatores de translação da window corrigidos.

--- CG-SGI/src/geometria/Mundo.cpp
+++ CG-SGI/src/geometria/Mundo.cpp
@@ -32,16 +32,16 @@ QList<ObjetoGeometrico*> Mundo::getObjetos() const {
 void Mundo::navegar(const Direcao direcao, const double fator) {
 	switch(direcao) {
 		case Direcao::CIMA:
-			this->window.transladar(0, -1 *fator);
-			break;
-		case Direcao::BAIXO:
 			this->window.transladar(0, fator);
 			break;
+		case Direcao::BAIXO:
+			this->window.transladar(0, -1 * fator);
+			break;
 		case Direcao::DIREITA:
-			this->window.transladar(-1 * fator, 0);
+			this->window.transladar(fator, 0);
 			break;
 		case Direcao::ESQUERDA:
-			this->window.transladar(fator, 0);
+			this->window.transladar(-1 * fator, 0);
 			break;
 	}
 }
