369693e64231598f238a8d9c2e06a006a65e86c3
Lucas Petry <lucasmaypetry@gmail.com>
2015-03-24 16:31:45 -0300

    Métodos implementados parcialmente na classe ArquivoOBJ.

--- CG-SGI/include/persistencia/ArquivoOBJ.h
+++ CG-SGI/include/persistencia/ArquivoOBJ.h
@@ -1,11 +1,56 @@
 #ifndef ARQUIVOOBJ_H_
 #define ARQUIVOOBJ_H_
 
+#include <QtCore/qlist.h>
+
+#include "excecao/ExcecaoArquivoInvalido.h"
+#include "geometria/ObjetoGeometrico.h"
+
+/**
+ * Arquivo Wavefront OBJ.
+ */
 class ArquivoOBJ {
 
 public:
-	ArquivoOBJ();
+	/**
+	 * Construtor.
+	 * @param nome nome completo do arquivo.
+	 */
+	ArquivoOBJ(const String& nome);
+
+	/**
+	 * Destrutor.
+	 */
 	virtual ~ArquivoOBJ();
+
+	/**
+	 * Escrever objetos no arquivo OBJ especificado.
+	 * @param objetos objetos a serem escritos no arquivo.
+	 */
+	void escreverObjetos(const QList<ObjetoGeometrico*> objetos) const;
+
+	/**
+	 * Carregar os objetos do arquivo OBJ.
+	 * @return lista contendo os objetos carregados.
+	 * @throws ExcecaoArquivoInvalido se o arquivo especificado não foi encontrado ou não possui um formato válido.
+	 */
+	QList<ObjetoGeometrico*> carregarObjetos() const
+			throw (ExcecaoArquivoInvalido);
+
+	/**
+	 * Verificar se o arquivo existe.
+	 * @return true se o arquivo existe.
+	 */
+	bool existe() const;
+
+protected:
+	/**
+	 * Remover o arquivo.
+	 * @return true se o arquivo foi removido.
+	 */
+	bool remover() const;
+
+	String nomeArquivo;
 
 };
 
--- CG-SGI/src/persistencia/ArquivoOBJ.cpp
+++ CG-SGI/src/persistencia/ArquivoOBJ.cpp
@@ -1,11 +1,59 @@
 #include "persistencia/ArquivoOBJ.h"
 
-ArquivoOBJ::ArquivoOBJ() {
-	// TODO Auto-generated constructor stub
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
 
+ArquivoOBJ::ArquivoOBJ(const String& nome) {
+	this->nomeArquivo = nome;
 }
 
-ArquivoOBJ::~ArquivoOBJ() {
-	// TODO Auto-generated destructor stub
+ArquivoOBJ::~ArquivoOBJ() {}
+
+void ArquivoOBJ::escreverObjetos(const QList<ObjetoGeometrico*> objetos) const {
+	this->remover();
+	std::fstream arquivo(this->nomeArquivo.c_str(), std::fstream::out | std::fstream::app);
+
+	if(!arquivo)
+		throw ExcecaoArquivoInvalido(this->nomeArquivo);
+
+	// TODO Escrever objetos no arquivo.
 }
 
+QList<ObjetoGeometrico*> ArquivoOBJ::carregarObjetos() const
+		throw (ExcecaoArquivoInvalido) {
+	std::fstream arquivo(this->nomeArquivo.c_str(), std::fstream::in);
+	std::stringstream buffer;
+
+	if(!arquivo)
+		throw ExcecaoArquivoInvalido(this->nomeArquivo);
+
+	buffer << arquivo.rdbuf();
+	arquivo.close();
+
+	// TODO Ler objetos do arquivo.
+	while(buffer) {
+		String t;
+		buffer >> t;
+		std::cout << "Line: " << t << std::endl;
+	}
+
+	return QList<ObjetoGeometrico*>();
+}
+
+bool ArquivoOBJ::existe() const {
+	std::fstream arquivo(this->nomeArquivo.c_str(), std::fstream::in);
+
+	if(arquivo)
+		return true;
+
+	return false;
+}
+
+bool ArquivoOBJ::remover() const {
+	if(remove(this->nomeArquivo.c_str()) != 0)
+		return false;
+
+	return true;
+}
