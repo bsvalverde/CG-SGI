8e2d3bfeb5f37b010b18e5eec6e8eadb87cee90f
Lucas Petry <lucasmaypetry@gmail.com>
2015-04-07 00:05:48 -0300

    Clipping de Cohen-Sutherland corrigido.

--- CG-SGI/src/gui/clipping/ClippingCohenSutherland.cpp
+++ CG-SGI/src/gui/clipping/ClippingCohenSutherland.cpp
@@ -18,33 +18,42 @@ bool ClippingCohenSutherland::clipReta(Reta* const reta) const {
 	std::cout << (test | 0b1) << std::endl;
 	 */
 
+	// Region Code = Topo [0] Fundo [1] Direita [2] Esquerda [3]
+
 	int rc1[4] = { 0, 0, 0, 0 };
+
 	if (p1->getX() > xvMax) {
-		rc1[0] = 1;
+		rc1[2] = 1;
 	} else if (p1->getX() < xvMin) {
+		rc1[3] = 1;
+	}
+
+	if (p1->getY() > yvMax) {
+		rc1[0] = 1;
+	} else if (p1->getY() < yvMin) {
 		rc1[1] = 1;
 	}
-	if (p1->getY() > yvMax) {
-		rc1[3] = 1;
-	} else if (p1->getY() < yvMin) {
-		rc1[4] = 1;
-	}
+
 	int rc2[4] = { 0, 0, 0, 0 };
+
 	if (p2->getX() > xvMax) {
-		rc2[0] = 1;
+		rc2[2] = 1;
 	} else if (p2->getX() < xvMin) {
-		rc2[1] = 1;
+		rc2[3] = 1;
 	}
 	if (p2->getY() > yvMax) {
-		rc2[3] = 1;
+		rc2[0] = 1;
 	} else if (p2->getY() < yvMin) {
-		rc2[4] = 1;
+		rc2[1] = 1;
 	}
+
 	if ((rc1[0] == 1 && rc2[0] == 1) || (rc1[1] == 1 && rc2[1] == 1)
 			|| (rc1[2] == 1 && rc2[2] == 1) || (rc1[3] == 1 && rc2[3] == 1))
 		return false;
-	if (rc1 == rc2)
+
+	if ((rc1[0] == rc2[0]) && (rc1[1] == rc2[1]) && (rc1[2] == rc2[2]) && (rc1[3] == rc2[3]))
 		return true;
+
 	if (rc1[0] == 1) {
 		if (!this->clippingTopo(p1, coefAngular))
 			return false;
