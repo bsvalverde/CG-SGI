256093b8b938ebb32ec430c7e9b3c1f4f4ca7cfd
bernardo <bsvalverde@gmail.com>
2015-04-07 22:40:46 -0300

    nao sei o que fazer para clipar poligonos

--- CG-SGI/src/gui/clipping/Clipping.cpp
+++ CG-SGI/src/gui/clipping/Clipping.cpp
@@ -1,31 +1,45 @@
 #include "gui/clipping/Clipping.h"
 
-Clipping::Clipping(const double xvMin, const double xvMax, const double yvMin, const double yvMax) {
+Clipping::Clipping(const double xvMin, const double xvMax, const double yvMin,
+		const double yvMax) {
 	this->xvMin = xvMin;
 	this->xvMax = xvMax;
 	this->yvMin = yvMin;
 	this->yvMax = yvMax;
 }
 
-Clipping::~Clipping() {}
+Clipping::~Clipping() {
+}
 
 bool Clipping::clip(ObjetoGeometrico* const objeto) const {
-	switch(objeto->getTipo()) {
-		case ObjetoGeometrico::POLIGONO:
-			return this->clipPoligono((Poligono* const) objeto);
-		case ObjetoGeometrico::PONTO:
-			return this->clipPonto((Ponto* const) objeto);
-		case ObjetoGeometrico::RETA:
-			return this->clipReta((Reta* const) objeto);
-		default:
-			return false;
+	switch (objeto->getTipo()) {
+	case ObjetoGeometrico::POLIGONO:
+		return this->clipPoligono((Poligono* const ) objeto);
+	case ObjetoGeometrico::PONTO:
+		return this->clipPonto((Ponto* const ) objeto);
+	case ObjetoGeometrico::RETA:
+		return this->clipReta((Reta* const ) objeto);
+	default:
+		return false;
 	}
 }
 
 bool Clipping::clipPoligono(Poligono* const poligono) const {
-	return true;
+	QList<Ponto*> pontos = poligono->getPontosObjeto();
+	Poligono copia = poligono->clonar();
+	int cont = 0;
+	int tam = pontos.size();
+	for (int i = 0; i < tam; i++) {
+		Ponto* p1 = pontos.at(i);
+		Ponto* p2 = pontos.at((i+1)%tam);
+		Reta reta("olá", *p1, *p2);
+		if (this->clipReta(&reta))
+			cont++;
+	}
+	return cont == 0;
 }
 
 bool Clipping::clipPonto(Ponto* const ponto) const {
-	return (ponto->getX() > xvMin && ponto->getX() < xvMax && ponto->getY() > yvMin && ponto->getY() < yvMax);
+	return (ponto->getX() > xvMin && ponto->getX() < xvMax
+			&& ponto->getY() > yvMin && ponto->getY() < yvMax);
 }
