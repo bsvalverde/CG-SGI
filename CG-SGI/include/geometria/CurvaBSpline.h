e0bed815269e304ed4d9d34eca0745b12a0adbd8
Lucas Petry <lucasmaypetry@gmail.com>
2015-04-12 00:38:39 -0300

    Classe da curva B-Spline criada.

---
+++ CG-SGI/include/geometria/CurvaBSpline.h
@@ -0,0 +1,22 @@
+#ifndef CURVABSPLINE_H_
+#define CURVABSPLINE_H_
+
+/**
+ * Curva B-Spline.
+ */
+class CurvaBSpline {
+
+public:
+	/**
+	 * Construtor.
+	 */
+	CurvaBSpline();
+
+	/**
+	 * Destrutor.
+	 */
+	virtual ~CurvaBSpline();
+
+};
+
+#endif /* CURVABSPLINE_H_ */
---
+++ CG-SGI/src/geometria/CurvaBSpline.cpp
@@ -0,0 +1,6 @@
+#include "geometria/CurvaBSpline.h"
+
+CurvaBSpline::CurvaBSpline() {}
+
+CurvaBSpline::~CurvaBSpline() {}
+
